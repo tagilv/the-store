5c632c651e6b47620537dadc300dde904d69ff36
Viktor Tagil <viktor.tagil@gmail.com>
2025-10-05 12:43:43 +0200

    Add Shopify Storefront API client and test GraphQL queries

---
+++ lib/shopify/client.ts
@@ -0,0 +1,7 @@
+import { createStorefrontApiClient } from "@shopify/storefront-api-client";
+
+export const client = createStorefrontApiClient({
+  storeDomain: process.env.SHOPIFY_STORE_DOMAIN!,
+  apiVersion: "2023-10",
+  publicAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+});
---
+++ lib/shopify/queries.ts
@@ -0,0 +1,41 @@
+export const GET_PRODUCTS_QUERY = `
+  query getProducts($first: Int!) {
+    products(first: $first) {
+      edges {
+        node {
+          id
+          title
+          description
+          handle
+          tags
+          priceRange {
+            minVariantPrice {
+              amount
+              currencyCode
+            }
+          }
+          images(first: 1) {
+            edges {
+              node {
+                url
+                altText
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_SHOP_QUERY = `
+  query getShop {
+    shop {
+      name
+      primaryDomain {
+        host
+        url
+      }
+    }
+  }
+`;
