c826542dd5ac8f9d96a6e725931ca65b9c7bff8f
Viktor Tagil <viktor.tagil@gmail.com>
2025-10-05 17:14:18 +0200

    Update components to support null items

--- components/item-grid.tsx
+++ components/item-grid.tsx
@@ -2,9 +2,12 @@ import { Item } from "@/lib/data";
 import { ItemGridClient } from "./item-grid-client";
 
 interface ItemGridProps {
-  items: Item[];
+  items: Item[] | null;
 }
 
 export function ItemGrid({ items }: ItemGridProps) {
+  if (!items) {
+    return <div>No items available</div>;
+  }
   return <ItemGridClient items={items} />;
 }
