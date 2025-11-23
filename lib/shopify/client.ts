ac5857981053893c561107fd00ec38b977749c8a
Viktor Tagil <viktor.tagil@gmail.com>
2025-11-23 14:54:19 +0100

    Update Shopify API version to 2025-10

--- lib/shopify/client.ts
+++ lib/shopify/client.ts
@@ -2,6 +2,6 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 
 export const client = createStorefrontApiClient({
   storeDomain: process.env.SHOPIFY_STORE_DOMAIN!,
-  apiVersion: "2023-10",
+  apiVersion: "2025-10",
   publicAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
 });
